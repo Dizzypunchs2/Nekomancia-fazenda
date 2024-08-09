f26576c2a69dd109ec31ee42029c935fe24613fa
Stephanie <stephaniebarbosa96@gmail.com>
2024-08-08 22:42:59 -0300

    08/08/2024: Carregando as PNGs e iniciando a animacao do Player.

--- src/player.py
+++ src/player.py
@@ -7,10 +7,12 @@ class Player (pygame.sprite.Sprite):
         super().__init__(group)
         
         self.import_assets()
+        self.status ='down_idle'
+        self.frame_index=0
+        
         
         # general setup
-        self.image =pygame.Surface((32,64))
-        self.image.fill('green')
+        self.image =self.animations[self.status][self.frame_index]
         self.rect= self.image.get_rect(center=pos)
         
         # movement attribute
@@ -33,23 +35,46 @@ class Player (pygame.sprite.Sprite):
             full_path = '../graphics/character/' + animation
             self.animations[animation] = import_folder(full_path)
         
+        
+    def animate(self,dt):
+        self.frame_index+=4*dt
+        if self.frame_index>= len(self.animations[self.status]):
+            self.frame_index=0
+            
+        self.image = self.animations[self.status][int(self.frame_index)]
+    
     def input(self):
         keys= pygame.key.get_pressed()
         
         if keys[pygame.K_UP]:
             self.direction.y= -1
+            self.status='up'
+            
         elif keys[pygame.K_DOWN]:
             self.direction.y= 1
+            self.status='down'
+            
         else:
             self.direction.y=0
                 
         if keys[pygame.K_RIGHT]:
             self.direction.x=1
+            self.status='right'
+            
         elif keys[pygame.K_LEFT]:
             self.direction.x=-1
+            self.status='left'
+            
         else:
             self.direction.x=0
             
+    def get_status(self):
+        # if trhe player is not moving:
+        if self.direction.magnitude()== 0:
+            # add_idle to the status
+            self.status= self.status.split('_')[0] +'_idle'
+        
+    
     def move(self,dt):
         # normalizing a vector
         if self.direction.magnitude()>0:
@@ -64,4 +89,6 @@ class Player (pygame.sprite.Sprite):
         
     def update(self, dt):
         self.input()
+        self.get_status()
         self.move(dt)
+        self.animate(dt)
--- src/support.py
+++ src/support.py
@@ -1,11 +1,15 @@
 from os import walk
+import pygame
 
 def import_folder(path):
     surface_list =[]
    
     for _, __, img_names in walk(path):
-        
-        print(img_names)
+        for image in img_names:
+            full_path= path +'/'+ image
+            image_surface= pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surface)
+            
      
     return surface_list
 
